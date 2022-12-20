2ae4bf0a43c43cf21f0d01cc1bf7a963f7c5fa62
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2022-12-20 19:25:56 -0300

    Att

--- Clinic_SYS_DBSetup.py
+++ Clinic_SYS_DBSetup.py
@@ -21,8 +21,8 @@ if conn is not None:
     cursor.execute('CREATE TABLE  admin (id serial, name VARCHAR(64)NOT NULL, password VARCHAR(64)NOT NULL, email VARCHAR(64)NOT NULL, cpf varchar(14) NOT NULL, position varchar(64) NOT NULL, PRIMARY KEY(id));')
     print('Table Admin Created!')
 
-    cursor.execute('CREATE TABLE  person (id serial, name VARCHAR(64)NOT NULL, email VARCHAR(64)NOT NULL, cpf varchar(14) NOT NULL, PRIMARY KEY(id));')
-    print('Table Person Created!')
+    # cursor.execute('CREATE TABLE  person (id serial, name VARCHAR(64)NOT NULL, email VARCHAR(64)NOT NULL, cpf varchar(14) NOT NULL, PRIMARY KEY(id));')
+    # print('Table Person Created!')
 
     # Create the 2 tables first than comment those lines and create this one.
     # cursor.execute('CREATE TABLE  reports (id serial, idPerson int, date_report date NULL, description varchar(128) NOT NULL, PRIMARY KEY(id)) inherits (person);')
