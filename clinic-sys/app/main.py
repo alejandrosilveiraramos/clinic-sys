436bcf08241100a35329acc4f85dfb9ce9857c56
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2022-12-20 19:43:48 -0300

    Subindo Doctos

--- clinic-sys/app/main.py
+++ clinic-sys/app/main.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def root():
-    return 'Root Start'
+@app.route('/home')
+def home_page():
+    return render_template('./components/home.html')
 
-app.run(debug=True)
+@app.route('/new_patient')
+def new_patient_page():
+    return render_template('./components/newPatient.html')
+
+@app.route('/list')
+def list_page():
+    return render_template('./components/list.html')
+
+@app.route('/more_info/<id_patient>')
+def more_info(id_patient):
+    return render_template('./components/moreInfoPatient.html', patient=id_patient)
+
+@app.route('/login')
+def login_page():
+    return render_template('./components/login.html')
+
+@app.route('/register')
+def register_page():
+    return render_template('./components/register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\thiago.franca\Documents\clinic-sys\venv
+set VIRTUAL_ENV=C:\Users\joao.oliveira\Documents\clinic-sys\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
